b115323a329fb2c00abde73dbf5b29d221ee3394
Jonatas <jonatasf1997@hotmail.com>
2022-10-18 17:19:47 -0300

    feat: add Header

--- src/App.tsx
+++ src/App.tsx
@@ -1,11 +1,13 @@
 import "./styles/global.css";
 
 import { BrowserRouter } from "react-router-dom";
+import { Header } from "./components";
 import { Routes } from "./routes";
 
 function App() {
   return (
     <BrowserRouter>
+      <Header />
       <Routes />
     </BrowserRouter>
   );
--- src/components/Header/index.tsx
+++ src/components/Header/index.tsx
@@ -1,3 +1,19 @@
+import { Link } from "react-router-dom";
+
 export function Header() {
-  return <div>Header</div>;
+  return (
+    <header className="flex items-center justify-evenly bg-gray-500  text-white ">
+      <section className="w-[500px] my-7 text-lg font-lg">
+        <Link to={"/"}>
+          <h2>Rockr Blog</h2>
+        </Link>
+      </section>
+      <section className="my-7 text-md font-md ">
+        <Link to={"/"} className="mx-3">
+          Posts
+        </Link>
+        <span className="mx-3">Contact</span>
+      </section>
+    </header>
+  );
 }
