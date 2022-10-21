8856f838f9e1bdea3ff93fe19b66970e394218d9
Jonatas <jonatasf1997@hotmail.com>
2022-10-20 23:36:06 -0300

    feat: add react hook

--- src/components/Input/index.tsx
+++ src/components/Input/index.tsx
@@ -1,21 +1,28 @@
-import { InputHTMLAttributes } from "react";
+import { ForwardRefRenderFunction, forwardRef } from "react";
 
-export interface IInputProps extends InputHTMLAttributes<HTMLInputElement> {
+export interface IInputProps {
   label: string;
 }
 
-export function Input(props: IInputProps) {
-  const { id, label } = props;
+const InputBase: ForwardRefRenderFunction<HTMLInputElement, any> = (
+  props,
+  ref
+) => {
+  const { label } = props;
 
   return (
     <div className="w-full mb-8">
-      <label htmlFor={id} className="font-sm text-sm">
+      <label htmlFor={label} className="font-sm text-sm">
         {label}
       </label>
       <input
-        className="w-full p-2 rounded-[4px] border-2 border-gray-500 placeholder:text-black placeholder:font-sm placeholder:text-xs placeholder:opacity-50"
+        id={label}
+        ref={ref}
         {...props}
+        className="w-full p-2 rounded-[4px] border-2 border-gray-500 placeholder:text-black placeholder:font-sm placeholder:text-xs placeholder:opacity-50"
       />
     </div>
   );
-}
+};
+
+export const Input = forwardRef(InputBase);
--- src/components/Modal/templates/Contact/index.tsx
+++ src/components/Modal/templates/Contact/index.tsx
@@ -1,9 +1,25 @@
 import Arrow from "./Arrow";
 import { Input } from "../../../Input";
+import { useForm } from "react-hook-form";
 import { useModal } from "../../../../context";
+
+interface IFormContact {
+  name: string;
+  email: string;
+  phone: string;
+  description: string;
+}
 
 export function ContactModal() {
   const { closeModal } = useModal();
+  const {
+    register,
+    getValues,
+    handleSubmit,
+    formState: { isSubmitting },
+  } = useForm<IFormContact>();
+
+  const submit = handleSubmit((data) => console.log(data));
 
   return (
     <main className="flex flex-col justify-center items-center w-full p-10">
@@ -17,16 +33,36 @@ export function ContactModal() {
         </button>
       </header>
       <section className="w-full px-20">
-        <form className="flex flex-col justify-center items-center">
-          <Input type="text" label="Name" placeholder="Fill your full name" />
+        <form
+          onSubmit={submit}
+          className="flex flex-col justify-center items-center"
+        >
+          <Input
+            label="Name"
+            placeholder="Fill your full name"
+            {...register("name")}
+          />
           <Input
             type="email"
             label="E-mail"
             placeholder="Fill a valid e-mail"
+            {...register("email")}
           />
-          <Input type="text" label="Phone" placeholder="Fill your fone" />
-          <Input type="" label="Post" placeholder="Hello..." />
-          <button className="flex justify-around items-center w-56 h-16 p-4 m-9 bg-gray-500 text-white text-sm rounded transition-all ease-in hover:bg-gray-800">
+          <Input
+            type="text"
+            label="Phone"
+            placeholder="Fill your fone"
+            {...register("phone")}
+          />
+          <Input
+            label="Post"
+            placeholder="Hello..."
+            {...register("description")}
+          />
+          <button
+            disabled={isSubmitting}
+            className="flex justify-around items-center w-56 h-16 p-4 m-9 bg-gray-500 text-white text-sm rounded transition-all ease-in hover:bg-gray-800 disabled:opacity-75"
+          >
             <Arrow />
             Submit
           </button>
