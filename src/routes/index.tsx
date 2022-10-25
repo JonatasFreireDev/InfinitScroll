1bc8a7faeab9fee5323bffc974abae4778722fb5
Jonatas Freire <Jonatasf1997@hotmail.com>
2022-10-25 17:07:17 -0300

    feat: change the way of how router dom works

--- src/routes/index.tsx
+++ src/routes/index.tsx
@@ -1,21 +1,30 @@
 import { Suspense, lazy } from "react";
-import { Routes as NativeRoutes, Route } from "react-router-dom";
+import {
+  createRoutesFromElements,
+  createBrowserRouter,
+  Route,
+} from "react-router-dom";
 
-import { Layout } from "../components";
+import { Layout, ScrollToTop } from "../components";
 
 const Main = lazy(() => import("../pages/Main"));
 const Post = lazy(() => import("../pages/Post"));
 const Error = lazy(() => import("../pages/Error"));
 
-export function Routes() {
-  return (
-    <Suspense fallback={<div>Carregando...</div>}>
-      <NativeRoutes>
-        <Route path="/" element={<Layout />} errorElement={<Error />}>
-          <Route index element={<Main />} />
-          <Route path="post/:postId" element={<Post />} />
-        </Route>
-      </NativeRoutes>
-    </Suspense>
-  );
-}
+export const router = createBrowserRouter(
+  createRoutesFromElements(
+    <Route
+      path="/"
+      element={
+        <Suspense fallback={<div>Carregando...</div>}>
+          <ScrollToTop />
+          <Layout />
+        </Suspense>
+      }
+      errorElement={<Error />}
+    >
+      <Route index element={<Main />} />
+      <Route path="post/:postId" element={<Post />} />
+    </Route>
+  )
+);
