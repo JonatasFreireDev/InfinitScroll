5e0b4c1303209a9757e90eec750251051a1a04a5
Jonatas <jonatasf1997@hotmail.com>
2022-12-05 21:59:58 -0300

    feat: animation in modal

--- src/components/Modal/index.tsx
+++ src/components/Modal/index.tsx
@@ -1,6 +1,7 @@
 import React, { ReactNode, useCallback } from "react";
 
 import { useModal } from "../../context";
+import { motion, AnimatePresence } from "framer-motion";
 
 interface IModalProps {
   className?: string;
@@ -14,21 +15,29 @@ export const Modal: React.FC<IModalProps> = ({ className, children }) => {
     closeModal();
   }, []);
 
-  if (statusModal) {
-    return (
-      <div
-        onClick={closeModalFunc}
-        className="fixed flex justify-center items-center bg-black bg-opacity-60 top-0 h-screen w-screen animate-appearFromNothing"
-      >
-        <section
-          className={`absolute bg-white rounded-[10px] min-h-[200px] w-[50%] sm:w-[90%] max-w-3xl animate-appearFromTop !opacity-100 ${className}`}
-          onClick={(e) => e.stopPropagation()}
+  return (
+    <AnimatePresence>
+      {statusModal && (
+        <motion.div
+          initial={{ opacity: 0 }}
+          animate={{ opacity: 1 }}
+          exit={{ opacity: 0 }}
+          transition={{ duration: 0.2 }}
+          onClick={closeModalFunc}
+          className="fixed flex justify-center items-center bg-black bg-opacity-60 top-0 h-screen w-screen"
         >
-          {children}
-        </section>
-      </div>
-    );
-  }
-
-  return null;
+          <motion.div
+            initial={{ translateY: "-100px", opacity: 0 }}
+            animate={{ translateY: "0px", opacity: 1 }}
+            exit={{ translateY: "-100px", opacity: 0 }}
+            transition={{ duration: 0.2 }}
+            className={`absolute bg-white rounded-[10px] min-h-[200px] w-[50%] sm:w-[90%] max-w-3xl !opacity-100 ${className}`}
+            onClick={(e) => e.stopPropagation()}
+          >
+            {children}
+          </motion.div>
+        </motion.div>
+      )}
+    </AnimatePresence>
+  );
 };
